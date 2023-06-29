56f62130b86688d7ac2fc3219e0da985c0ae6f5b
Sampada <sampada>
2023-06-29 14:42:07 -0700

    Initial Commit

---
+++ RecipeRaiders2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeRaiders2
+//
+//  Created by Sampada Baral on 6/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeRaiders2/RecipeRaiders2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeRaiders2App.swift
+//  RecipeRaiders2
+//
+//  Created by Sampada Baral on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeRaiders2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
